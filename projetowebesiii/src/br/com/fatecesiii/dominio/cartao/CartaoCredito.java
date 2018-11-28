c65f955b83805c0e4dcbe01fa018506a89b50926
Andre Valeriano <30508453+valleriano@users.noreply.github.com>
2018-11-28 18:52:52 -0200

    Teste git

--- projetowebesiii/src/br/com/fatecesiii/dominio/cartao/CartaoCredito.java
+++ projetowebesiii/src/br/com/fatecesiii/dominio/cartao/CartaoCredito.java
@@ -1,5 +1,25 @@
 package br.com.fatecesiii.dominio.cartao;
 
+import java.util.Date;
+
 public class CartaoCredito extends Cartao {
 
+	public void CriaCartao() throws Exception {
+		
+		Date dtCadastro;
+		String nome;
+		int tipoCartao;
+		
+		dtCadastro = getDataCadastro();
+		
+		if(dtCadastro != null) {
+			tipoCartao = 1;
+			nome = getNomeImpressoCartao();
+			
+			if(nome.isEmpty()) {
+				throw new Exception("Cartão Inválido");
+			}
+		}
+		
+	}
 }
